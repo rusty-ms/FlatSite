e23a2c130cab714fe8c787786216dc0ac3ef7011
Rusty <r@usty.ms>
2017-01-27 09:32:13 -0600

    Removing Stanley from navbar

--- resources/views/rustyms.blade.php
+++ resources/views/rustyms.blade.php
@@ -33,7 +33,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">STANLEY</a>
+          <a class="navbar-brand" href="/">Rusty</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
