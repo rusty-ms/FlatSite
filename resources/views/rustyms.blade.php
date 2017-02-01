fe6efd6888237c66b9b4fa554489e5ad011fed8b
Rusty <r@usty.ms>
2017-02-01 14:38:59 -0600

    Adding jquery and some tweaks

--- resources/views/rustyms.blade.php
+++ resources/views/rustyms.blade.php
@@ -87,17 +87,6 @@
         }
 
     </style>
-    <script>
-        $(document).ready(function() {
-            $(".btn-pref .btn").click(function () {
-                $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-                // $(".tab").addClass("active"); // instead of this do the below
-                $(this).removeClass("btn-default").addClass("btn-primary");
-            });
-        });
-    </script>
-
-
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -171,5 +160,17 @@
 <!-- JS -->
 <script src="https://use.fontawesome.com/7580b066b9.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
+
+<script>
+    $(document).ready(function() {
+        $(".btn-pref .btn").click(function () {
+            $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+            // $(".tab").addClass("active"); // instead of this do the below
+            $(this).removeClass("btn-default").addClass("btn-primary");
+        });
+    });
+</script>
+
 </body>
 </html>
