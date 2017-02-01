960550f0e786483b53b1ffe1227c18502ea8c2ce
Rusty <r@usty.ms>
2017-02-01 15:04:24 -0600

    Row and column tweaks

--- resources/views/rustyms.blade.php
+++ resources/views/rustyms.blade.php
@@ -90,7 +90,7 @@
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="/assets/css/bootstrap-social.css" />
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
@@ -98,11 +98,11 @@
 <body>
 <div class="container">
     <div class="row ">
-            <div class="col-xs-5 text-center"><a class="btn btn-social-icon btn-twitter"><span class="fa fa-twitter"></span></a></div>
-            <div class="col-xs-5 text-center"><a class="btn btn-social-icon btn-github"><span class="fa fa-github"></span></a></div>
-            <div class="col-xs-5 text-center"><a class="btn btn-social-icon btn-facebook"><span class="fa fa-facebook"></span></a></div>
-            <div class="col-xs-5 text-center"><a class="btn btn-social-icon btn-linkedin"><span class="fa fa-linkedin"></span></a></div>
-                <div class="col-xs-5 text-center"><a class="btn btn-social-icon btn-google"><span class="fa fa-google"></span></a></div>
+            <div class="col-xs-2 text-center"><a class="btn btn-social-icon btn-twitter"><span class="fa fa-twitter"></span></a></div>
+            <div class="col-xs-2 text-center"><a class="btn btn-social-icon btn-github"><span class="fa fa-github"></span></a></div>
+            <div class="col-xs-2 text-center"><a class="btn btn-social-icon btn-facebook"><span class="fa fa-facebook"></span></a></div>
+            <div class="col-xs-2 text-center"><a class="btn btn-social-icon btn-linkedin"><span class="fa fa-linkedin"></span></a></div>
+            <div class="col-xs-2 text-center"><a class="btn btn-social-icon btn-google"><span class="fa fa-google"></span></a></div>
     </div>
     <div class="row">
 
