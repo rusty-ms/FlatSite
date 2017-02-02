6302befb77dcb97e35c71a49465085e55551dd33
Rusty <r@usty.ms>
2017-02-02 16:26:02 -0600

    adding menu toggle to top of page

--- resources/views/pickupnwraa.blade.php
+++ resources/views/pickupnwraa.blade.php
@@ -103,6 +103,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
+                    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
                     <h1>Welcome to NWRAA's Pickup Soccer Site</h1>
                     <p><h2>What are pickup games?</h2>
                     <ol>
