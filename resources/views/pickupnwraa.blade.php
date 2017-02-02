e9dcfa7afafb48526c3567baded0b0f2f54a407e
Rusty <r@usty.ms>
2017-02-02 16:09:41 -0600

    Text tweaks and mailchimp form.

--- resources/views/pickupnwraa.blade.php
+++ resources/views/pickupnwraa.blade.php
@@ -45,7 +45,7 @@
                 <a href="https://drive.google.com/file/d/0B2W3rhUQY7Wma0Y3WHNnb1VmOWc/edit?usp=sharing">Field Map</a>
             </li>
             <li>
-                <a href="mailto:r@usty.ms">Contact</a>
+                <a href="mailto:r@usty.ms">Contact Rusty</a>
             </li>
             <li>
                 <a href="http://www.usyouthsoccer.org/coaches/PolicyonPlayersandPlayingRules/">Rules</a>
@@ -57,7 +57,41 @@
                 <a href="https://www.google.com/maps/place/NWRAA+soccer+complex/@32.3840938,-90.0509101,17z/data=!4m5!3m4!1s0x0000000000000000:0x66ae7b69c69df9bf!8m2!3d32.3844113!4d-90.0503457">Directions</a>
             </li>
             <li>
-                <a href="#">Playing</a>
+                <a href="https://docs.google.com/document/d/1HSpYSfP8_eAg3hlYml35CgGu5mrG4w80yoZ5_twMNLo/pub">Are we playing? Click here!</a>
+            </li>
+            <li>
+                <!-- Begin MailChimp Signup Form -->
+                <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+                <style type="text/css">
+                    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                </style>
+                <div id="mc_embed_signup">
+                    <form action="//nwraasoccer.us14.list-manage.com/subscribe/post?u=8251f3053424e078255bb209f&amp;id=1ed55297c0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                            <h2>Subscribe to our mailing list</h2>
+                            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                            <div class="mc-field-group">
+                                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                                </label>
+                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                            </div>
+                            <div class="mc-field-group">
+                                <label for="mce-MMERGE3">Name </label>
+                                <input type="text" value="" name="MMERGE3" class="" id="mce-MMERGE3">
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8251f3053424e078255bb209f_1ed55297c0" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                        </div>
+                    </form>
+                </div>
+                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[3]='MMERGE3';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                <!--End mc_embed_signup-->
             </li>
         </ul>
     </div>
@@ -97,6 +131,7 @@
                     <li>Sunday afternoons - Check the sidebar to the left for times. They vary due to weather, temperature, and events.</li>
                     <li>All ages are welcome, but attempts will be made to split stronger & older ages with younger ages due to safety.</li>
                     <li>We will always play 4 12-15 minute quarters to give everyone a chance to rest.</li>
+                        <li>Fields will change occassionally depending on field usage. Main fields are 3 and 8. Check field map on the left.</li>
                     </ol>
                     <h2>I'm an older adult or I'm out of share - Can I keep up?</h2>
                         <ol>
