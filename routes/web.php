c2da6a57ae5fe8dad66238c30f7ce5dfaa127b77
Rusty <rusty-ms@users.noreply.github.com>
2017-02-07 08:58:43 -0600

    Backslash our quotes

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,6 @@ Route::group(['domain' => 'pickup.nwraasoccer.com'], function () {
 });
 Route::group(['domain' => 'idealtechllc.com'], function () {
     Route::get('/', function () {
-        return "<center><b>Ideal Technologies is closed for business at this time. If you'd like to contact Rusty, please visit <a href="http://rusty.ms">http://rusty.ms</a>. If you are looking for IT services, please visit <a href="http://pileum.com">Pileum</a>.</b></center>";
+        return "<center><b>Ideal Technologies is closed for business at this time. If you'd like to contact Rusty, please visit <a href=\"http://rusty.ms\">http://rusty.ms</a>. If you are looking for IT services, please visit <a href=\"http://pileum.com\">Pileum</a>.</b></center>";
     });
 });
